9c63a47cd9badc8d8abb8f21c0de6d32ec38cf74
Green Van (ROCIO CARRILLO) <greenvanwebmaster@gmail.com>
2021-05-27 13:41:16 +0200

    Exercises 2.1 to 2.3

--- part2/courseinfo/README.md
+++ part2/courseinfo/README.md
@@ -7,8 +7,62 @@ Source files can be found in ['courseinfo/src'](https://github.com/greenvan/full
 
 App is on `App.js` file.
 
-## Exercise 2.1: Course information, step 6
+## Exercises 2.1 to 2.3*: Course information, steps 6, 7 and 8
 
 ```js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Total = ({ parts }) =>
+  <p><b>
+    Total of {
+      parts.reduce((total, part) => total + part.exercises, 0)
+    } exercises
+  </b></p>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ parts }) =>
+  <div>
+    {parts.map((part, i) => <Part key={i} part={part} />)}
+  </div>
+
+const Course = ({ course }) =>
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 ```
--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,51 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = ({course}) =>  <h1>{course.name}</h1>
-  
-const Total = ({course}) => 
-    <p>Number of exercises 
-      {course.parts[0].exercises + 
-      course.parts[1].exercises + 
-      course.parts[2].exercises}
-      </p>
+const Header = ({ name }) => <h1>{name}</h1>
 
-const Part = ({part}) => <p>{part.name} {part.exercises}</p>    
+const Total = ({ parts }) =>
+  <p><b>
+    Total of {
+      parts.reduce((total, part) => total + part.exercises, 0)
+    } exercises
+  </b></p>
 
-const Content = ({course}) => 
-    <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
-    </div>
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+//TODO not safe enough to use index of the array
+const Content = ({ parts }) =>
+  <div>
+    {parts.map((part, i) => <Part key={i} part={part} />)}
+  </div>
+
+const Course = ({ course }) =>
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
 
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
